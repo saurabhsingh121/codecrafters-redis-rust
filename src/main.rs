bbd78c74b8f409fa5b1ffb938d291f729379497c
saurabh-router <saurabh@routerprotocol.com>
2024-09-08 01:25:32 +0530

    respond to ping

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::TcpListener
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                
+                let mut buf = [0;512];
+                stream.read(&mut buf).unwrap();
+
+                stream.write(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
